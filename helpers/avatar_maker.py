696359796a6aed510f1984d5cd9edab391540428
sieczkah <huberts94@gmail.com>
2023-06-03 23:22:05 +0200

    Implemented random avatar in avatar_maker

--- helpers/avatar_maker.py
+++ helpers/avatar_maker.py
@@ -6,11 +6,19 @@ They also have a webpage. Check it out!
 https://multiavatar.com/
 """
 
+from random import choices
+from string import ascii_letters
+
 from multiavatar.multiavatar import multiavatar
 
+ASCII = ascii_letters + "1234567890!@#$%"
 
-def create_avatar(avatar_name):
+
+def create_avatar(avatar_name="random"):
     avatar_name = str(avatar_name)
+    if avatar_name == "random":
+        avatar_name = "".join(choices(ASCII, k=16))
+
     svg_code = multiavatar(avatar_name, None, None)
 
     return svg_code
